8b6ab040c20a0a56230b4156af8ee88d7e431831
Dovar <dovarabdulhanonov@gmail.com>
2021-04-10 15:52:32 +0500

    Добавлен register и deposit

--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -19,10 +19,50 @@ func (e *errorString) Error() string {
 
 
 var ErrAccountNotFound = errors.New("account not found")
+var ErrAccountMustBePositive = errors.New("Amount must be greater than zero")
+var ErrPhoneRegistered = errors.New("phone already registered")
 
 type Service struct{
 	nextAccountID int64
 	accounts [] *types.Account
+}
+
+func (s *Service) RegisterAccount(phone types.Phone) (*types.Account,error){
+
+	for _, account := range s.accounts {
+		if account.Phone == phone {
+			return nil,ErrPhoneRegistered
+		}
+	}
+	s.nextAccountID++
+	account := &types.Account{
+		ID: s.nextAccountID,
+		Phone: phone,
+		Balance: 0,
+	}
+	s.accounts = append(s.accounts, account)
+
+	return account,nil
+}
+func (s *Service) Deposit(accountID int64 , amount types.Money)error{
+	if amount <= 0 {
+		return ErrAccountMustBePositive
+	}
+	var account *types.Account
+	for _, acc := range s.accounts {
+		
+		if acc.ID == accountID{
+			account = acc
+			break
+		}
+
+	}
+	if account == nil{
+		return ErrAccountNotFound
+	}
+
+	account.Balance += amount
+	return nil
 }
 
 
