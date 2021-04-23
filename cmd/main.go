581e339dac1add1a456336317076130faddb26a6
Dovar <dovarabdulhanonov@gmail.com>
2021-04-23 12:22:51 +0500

    Export and Import

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	
+	"log"
 
 	"github.com/Dovar001/wallet/pkg/wallet"
 )
@@ -13,13 +13,32 @@ func main() {
  svc:= &wallet.Service{}
 
 
- //svc.RegisterAccount("+992909796600")
- //svc.RegisterAccount("+992927592402")
-//svc.RegisterAccount("+992000000000")
-//svc.RegisterAccount("+992111111111")
-//// svc.RegisterAccount("+992222222222")
+ svc.RegisterAccount("+992909796600")
+ svc.RegisterAccount("+992927592402")
+svc.RegisterAccount("+992000000000")
+svc.RegisterAccount("+9920000001200")
+
  //svc.ExportToFile("../data/export.txt")
- svc.ImportFromFile(".../data/export.txt")
+ //svc.ImportFromFile(".../data/export.txt")
+ svc.Deposit(1,10_000)
+ svc.Deposit(2, 234_000)
+
+pay,err:= svc.Pay(1, 500, "Машина")
+
+if err != nil {
+	log.Print(err)
+	return
+}
+
+
+ svc.FavoritePayment(pay.ID, "Довар")
+ 
+ svc.Export("../data")
+
+
+
+ 
+
 
 
  }
--- pkg/wallet/service.go
+++ pkg/wallet/service.go
@@ -357,7 +357,303 @@ data := string(content)
 
 
 
+func (s *Service) Export(dir string) error{
+    
 
+	if len(s.accounts)>0 {
+
+		file,err := os.OpenFile(dir + "/accounts.dump", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+
+if err != nil {
+	log.Print(err)
+	return err
+}
+defer func ()  {
+		
+	if cerr := file.Close(); cerr!= nil {
+		if err == nil {
+			cerr=err
+		}
+	}
+}()
+accstr:=""
+
+	
+
+	for _, account := range s.accounts {
+
+		accstr+= strconv.Itoa(int(account.ID))+";"
+		accstr+=string(account.Phone)+";"
+		accstr+=strconv.Itoa(int(account.Balance))+ "\n"
+		
+	}
+
+	file.WriteString(accstr)
+	}
+	
+	if len(s.payments)>0 {
+	fil,err := os.OpenFile(dir + "/payments.dump", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	defer func ()  {
+			
+		if cerr := fil.Close(); cerr!= nil {
+			if err == nil {
+				cerr=err
+			}
+		}
+	}()
+
+	paystr:=""
+
+	
+
+	for _, payment := range s.payments {
+
+		 paystr+=string(payment.ID) + ";"
+
+ 		 paystr+=strconv.Itoa(int(payment.AccountID))+ ";"
+
+		 paystr+= strconv.Itoa(int(payment.Amount))+ ";"
+
+		 paystr+= string(payment.Category)+ ";"
+
+         paystr+=string(payment.Status)+ "\n"
+	}
+	fil.WriteString(paystr)
+}
+
+if len (s.favorites) > 0 {
+
+	files,err := os.OpenFile(dir + "/favorites.dump", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0666)
+
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	defer func ()  {
+			
+		if cerr := files.Close(); cerr!= nil {
+			if err == nil {
+				cerr=err
+			}
+		}
+	}()
+
+	favstr := ""
+
+	
+
+	
+	for _, favorite := range s.favorites {
+
+		favstr+= favorite.ID + ";"
+		favstr+= strconv.Itoa(int(favorite.AccountID))+";"
+		favstr+= favorite.Name + ";"
+		favstr+= strconv.Itoa(int(favorite.Amount)) + ";"
+		favstr+= string(favorite.Category) + "\n"
+
+		
+	}
+	files.WriteString(favstr)
+}
+
+	
+
+	return nil
+}
+
+
+func (s *Service)  Import(dir string) error{
+	
+	file,err := os.OpenFile(dir + "/accounts.dump", os.O_WRONLY|os.O_TRUNC, 0666)
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+content := make([] byte,0)
+buf := make([]byte,4)
+
+for{
+
+	read,err := file.Read(buf)
+
+	if err == io.EOF{
+    
+		content=append(content, buf[:read]...)
+		break
+
+	}
+
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	content = append(content, buf[:read]...)
+
+}
+data := string(content)
+
+
+  accounts:=strings.Split(data,"\n")
+  accounts = accounts[:len(accounts)-1]
+
+  for _, account := range accounts {
+
+	splits := strings.Split(account, ";")
+
+	id,err := strconv.Atoi(splits[0])
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+
+	phone := splits[1]
+
+	balance,err := strconv.Atoi(splits[2])
+	if err != nil{
+		log.Print(err)
+		return err
+	}  
+
+	  s.accounts=append(s.accounts, &types.Account{
+		  ID: int64(id),
+		  Phone: types.Phone(phone),
+		  Balance: types.Money(balance),
+	  })
+  }
+//Payments============================================================
+  fil,err := os.OpenFile(dir + "/payments.dump", os.O_WRONLY|os.O_TRUNC, 0666)
+  if err != nil {
+	  log.Print(err)
+	  return err
+  }
+
+cont := make([] byte,0)
+buff := make([]byte,4)
+
+for{
+
+  read,err := fil.Read(buff)
+
+  if err == io.EOF{
+  
+	  cont=append(cont, buff[:read]...)
+	  break
+
+  }
+
+  if err != nil {
+	  log.Print(err)
+	  return err
+  }
+
+  cont = append(cont, buff[:read]...)
+
+}
+dat := string(cont)
+
+
+payments:=strings.Split(dat,"\n")
+payments = payments[:len(payments)-1]
+
+for _, payment := range payments {
+
+  splits := strings.Split(payment, ";")
+
+  id := splits[0]
+
+  accountid,err := strconv.Atoi(splits[1]) 
+  if err != nil {
+	  log.Print(err)
+	  return err
+  }
+
+  amount,err := strconv.Atoi(splits[2])
+  if err != nil{
+	  log.Print(err)
+	  return err
+  }  
+
+  category := splits[3]
+
+  status := splits[4]
+
+	s.payments=append(s.payments, &types.Payment{
+		ID: id,
+		AccountID:int64(accountid) ,
+		Amount: types.Money(amount),
+		Category: types.PaymentCategory(category),
+		Status: types.PaymentStatus(status),
+	})
+}
+
+//Favorites=================================================
+
+files,err :=os.OpenFile(dir + "/favorites.dump", os.O_WRONLY|os.O_TRUNC, 0666)
+
+if err != nil {
+	log.Print(err)
+	return err
+}
+
+contents := make([] byte ,0)
+buffs := make([]byte , 4)
+
+for {
+read,err := files.Read(buffs)
+
+if err == io.EOF{
+	contents = append(contents, buffs[:read]...)
+}
+if err != nil{
+	log.Print(err)
+	return err
+}
+contents = append(contents, buffs[:read]...)
+
+}
+
+dates := string(contents)
+
+favorites:= strings.Split(dates, "\n")
+favorites = favorites[:len(payments)-1]
+
+  for _, favorite := range favorites {
+
+	splits:= strings.Split(favorite, ";")
+ 
+	id := splits[0]
+	accountid,err := strconv.Atoi(splits[1])
+	if err != nil {
+		log.Print(err)
+		return err
+	}
+	name := splits[2]
+	amount,err := strconv.Atoi(splits[3])
+	category:= types.PaymentCategory(splits[4])
+
+
+
+	s.favorites=append(s.favorites, &types.Favorite{
+		ID: id,
+		AccountID:int64(accountid) ,
+		Name: name,
+		Amount: types.Money(amount),
+		Category: types.PaymentCategory(category),
+		
+	})
+
+	  
+  }
+ return nil
+  
+  }
 
 
 
